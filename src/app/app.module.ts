7b2b71172f0abf325b98aaaeb4246b6157f1f99c
Pete Burkindine <pete.burkindine@semanticbits.com>
2019-06-14 09:26:49 -0500

    Add autocomplete for comparison

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import {
   MatOptionModule,
   MatSelectModule,
-  MatFormFieldModule
+  MatFormFieldModule,
+  MatAutocompleteModule,
+  MatInputModule
 } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -14,7 +16,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatAutocompleteModule,
     MatFormFieldModule,
+    MatInputModule,
     MatOptionModule,
     MatSelectModule
   ],
